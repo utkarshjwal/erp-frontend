c9bb15ac785822f0fa2e45caf6fb0c08afe78368
Utkarsh Jaiswal <utkarshjaiswal2580@gmail.com>
2024-02-14 00:26:43 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {ChakraProvider} from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <>
+        <ChakraProvider >
+        </ChakraProvider>
+        </>
+  )
+}
+
+export default App
